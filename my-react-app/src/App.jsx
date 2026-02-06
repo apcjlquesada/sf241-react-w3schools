25b55b62c684f94f8046ea0a1201c8f86bd1eb80
Joe Gene Quesada <52223592+apcjlquesada@users.noreply.github.com>
2026-02-06 06:34:56 +0000

    Task 02 update your Vite-React website with sample code from W3Schools

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello SF241 WEBPROG!</h1>
+      <h2> Welcome and have fun with React!  </h2>
+    </div>
+  );
+}
+
+export default App;
