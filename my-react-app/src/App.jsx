af21eb0eaa3e904d0505b652b32cfdd343eb0a54
Joe Gene Quesada <52223592+apcjlquesada@users.noreply.github.com>
2026-02-06 14:55:41 +0800

    Add hosting information to App component

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -3,6 +3,7 @@ function App() {
     <div className="App">
       <h1>Hello SF241 WEBPROG!</h1>
       <h2> Welcome and have fun with React!  </h2>
+      <h3>Hosted on Vercel.com</h3>
     </div>
   );
 }
